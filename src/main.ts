666ae2d966ea09c644b795a6857f4a8a66360996
kostjakuris <kostjn04e@gmail.com>
2026-03-19 16:29:01 +0200

    fix cookieparser import

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
